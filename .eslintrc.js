a915204dac4ebf1b2f9ae6c8f326e6e51d01320a
mariotolucas <32984499+mariotolucas@users.noreply.github.com>
2021-10-03 13:04:54 -0300

    Deleting unnecessary files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
     browser: true,
+    commonjs: true,
     es2021: true
   },
   extends: [
@@ -11,12 +12,13 @@ module.exports = {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: 12,
-    sourceType: 'module'
+    ecmaVersion: 12
   },
   plugins: [
     'react'
   ],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
   }
 }
